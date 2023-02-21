bd08125100a601b51d10778fe24aaacdc62617d7
Salvador <salvadorpm@gmail.com>
2023-02-21 17:38:14 -0600

    Commit hasta desafío 4

--- src/app/clientes/cliente/cliente.component.ts
+++ src/app/clientes/cliente/cliente.component.ts
@@ -1,39 +1,45 @@
 import { Component } from '@angular/core';
 
-interface ICliente{
+interface ICliente {
   nombre: string;
-  edad:number;
+  edad: number;
 }
-
 
 @Component({
   selector: 'app-cliente',
   templateUrl: './cliente.component.html',
-  styleUrls: ['./cliente.component.css']
+  styleUrls: ['./cliente.component.css'],
 })
 export class ClienteComponent {
+  listaClientes: ICliente[] = [
+    {
+      nombre: 'Juan Perez',
+      edad: 50,
+    },
+    {
+      nombre: 'Ana Rodriguez',
+      edad: 30,
+    },
+    {
+      nombre: 'Marcos Romero',
+      edad: 45,
+    },
+  ];
 
-  listaClientes : ICliente[]=[
-    {
-      nombre:"Juan Perez",
-      edad:50
-    },
-    {
-      nombre:"Ana Rodriguez",
-      edad:30
-    },
-    {
-      nombre:"Marcos Romero",
-      edad:45
-    },
-  ]
-  
-  nuevoCliente : ICliente = {
-    nombre:"",
-    edad: 0
-  }
+  nuevoCliente: ICliente = {
+    nombre: '',
+    edad: 0,
+  };
 
-  agregar():void{
-    console.log(this.nuevoCliente);
+  agregar(): void {
+    //console.log(this.nuevoCliente);
+    if (this.nuevoCliente.nombre.trim().length === 0) {
+      return;
+    }
+    this.listaClientes.push(this.nuevoCliente);
+    this.nuevoCliente = {
+      nombre: '',
+      edad: 0,
+    };
   }
 }
